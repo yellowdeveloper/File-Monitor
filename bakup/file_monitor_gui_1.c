effa9141174e6a2b589f3cbd30fcaaab7e5a65d8
LeeMinkyu <yellowdeveloper25@gmail.com>
2024-12-16 06:07:05 +0900

    onclick directory error fix5

--- bakup/file_monitor_gui_1.c
+++ bakup/file_monitor_gui_1.c
@@ -15,45 +15,32 @@
 #include <gtk/gtk.h>
 #include <glib.h>
 
-// define error code
 #define EXT_SUCCESS 0
 #define EXT_ERR_TOO_FEW_ARGS 1
 #define EXT_ERR_INIT_INOTIFY 2
 #define EXT_ERR_ADD_WATCH 3
-#define EXT_ERR_BASE_PATH_NULL 4
 #define EXT_ERR_READ_INOTIFY 5
-#define EXT_ERR_LIBNOTIFY 6
+#define EXT_ERR_CONFIG_FILE 6
 
-// global variables 
-int IeventStatus = -1; // inotify watch status
-int IeventQueue = -1; // inotify waiting queue
-char* ProgramTitle = "file_monitor"; // title of program
-FILE* logFile = NULL; // log file pointer
-const char* filteredExtension = NULL; // file extension to filter
-// int emailEnabled = 0; // email notificaton activate status
-// const char* emailRecipient = NULL; // email receiver
-const char* logFilePath = NULL; // log file path
-time_t lastEventTime = 0; // last event occur time
-GtkTextBuffer *log_buffer = NULL; // log text buffer
-GtkWidget* file_list = NULL; // directory to monitor
-char monitored_path[512];
+int IeventStatus = -1;
+int IeventQueue = -1;
+char* ProgramTitle = "file_monitor";
+FILE* logFile = NULL;
+GtkTextBuffer *log_buffer = NULL;
+GtkWidget* file_list = NULL;
+char monitoredDirs[512][512];
+int monitoredDirCount = 0;
+char logFilePath[512] = "";
 
-// recurrent function and log file size check
-void rotate_log_file(const char* logPath) {
-    if (logFile) {
-        fclose(logFile);
-    }
-
-    // open new log file
-    logFile = fopen(logPath, "a");
+void init_log_file(const char* path) {
+    logFile = fopen(path, "a");
     if (!logFile) {
         perror("Error opening log file");
         exit(EXIT_FAILURE);
     }
-    printf("Log file created/opened at: %s\n", logPath); // ����� �޽���
+    printf("Log file initialized at: %s\n", path);
 }
 
-// callback func to use in g_idle_add() func
 gboolean update_log_buffer(gpointer data) {
     const char* eventMessage = (const char*)data;
 
@@ -65,37 +52,51 @@ gboolean update_log_buffer(gpointer data) {
     }
 
     g_free(data);
-
     return FALSE;
 }
 
-// event logging function
 void log_event(const char* eventMessage) {
     if (logFile) {
-         fprintf(logFile, "Event: %s\n", eventMessage); 
+        fprintf(logFile, "%s\n", eventMessage);
         fflush(logFile);
     }
 
-    // duplicate string and add to main thread
     g_idle_add(update_log_buffer, g_strdup(eventMessage));
 }
 
-// signal handle function: clean up after program ends
-void signal_handler(int signal) {
-    printf("Signal received: cleaning up\n");
+void read_config(const char* configPath) {
+    config_t cfg;
+    config_init(&cfg);
 
-    // remove inotify watch
-    if (inotify_rm_watch(IeventQueue, IeventStatus) == -1) {
-        fprintf(stderr, "Error removing from watch queue!\n");
-    }
-    close(IeventQueue); // ends inotify waiting queue
-
-    // close log file
-    if (logFile) {
-        fclose(logFile);
+    if (!config_read_file(&cfg, configPath)) {
+        fprintf(stderr, "Error reading config file %s: %s\n", configPath, config_error_text(&cfg));
+        config_destroy(&cfg);
+        exit(EXT_ERR_CONFIG_FILE);
     }
 
-    exit(EXIT_SUCCESS);
+    const char* logPath = NULL;
+    if (config_lookup_string(&cfg, "log_file", &logPath)) {
+        strncpy(logFilePath, logPath, sizeof(logFilePath));
+    } else {
+        fprintf(stderr, "Missing 'log_file' in config file\n");
+        config_destroy(&cfg);
+        exit(EXT_ERR_CONFIG_FILE);
+    }
+
+    config_setting_t* directories = config_lookup(&cfg, "monitor_directories");
+    if (directories && config_setting_is_array(directories)) {
+        monitoredDirCount = config_setting_length(directories);
+        for (int i = 0; i < monitoredDirCount && i < 512; ++i) {
+            const char* dir = config_setting_get_string_elem(directories, i);
+            strncpy(monitoredDirs[i], dir, sizeof(monitoredDirs[i]));
+        }
+    } else {
+        fprintf(stderr, "Missing or invalid 'monitor_directories' in config file\n");
+        config_destroy(&cfg);
+        exit(EXT_ERR_CONFIG_FILE);
+    }
+
+    config_destroy(&cfg);
 }
 
 void update_file_list(const char* path) {
@@ -106,7 +107,6 @@ void update_file_list(const char* path) {
     if (dir) {
         struct dirent* entry;
         while ((entry = readdir(dir)) != NULL) {
-            // exclude '.' & '..'
             if (strcmp(entry->d_name, ".") != 0 && strcmp(entry->d_name, "..") != 0) {
                 GtkTreeIter iter;
                 gtk_list_store_append(store, &iter);
@@ -117,52 +117,26 @@ void update_file_list(const char* path) {
     }
 }
 
-// event handle fucntion
 void process_event(const struct inotify_event* watchEvent) {
-    if (watchEvent->len > 0) { // handle if file has a name
+    if (watchEvent->len > 0) {
         char notificationMessage[512];
         char eventTime[64];
         time_t currentTime = time(NULL);
 
-        // format occur time
         strftime(eventTime, sizeof(eventTime), "%Y-%m-%d %H:%M:%S", localtime(&currentTime));
-        snprintf(notificationMessage, sizeof(notificationMessage), "[%s] File %s: ", eventTime, watchEvent->name); // create message include time
+        snprintf(notificationMessage, sizeof(notificationMessage), "[%s] File %s: ", eventTime, watchEvent->name);
 
-        // add message by event type
         if (watchEvent->mask & IN_CREATE) {
             strcat(notificationMessage, "created");
-            g_idle_add((GSourceFunc)update_file_list, g_strdup(monitored_path));
-        }
-        else if (watchEvent->mask & IN_DELETE) {
+        } else if (watchEvent->mask & IN_DELETE) {
             strcat(notificationMessage, "deleted");
-            g_idle_add((GSourceFunc)update_file_list, g_strdup(monitored_path));
-        }
-        else if (watchEvent->mask & IN_ACCESS) {
-            strcat(notificationMessage, "accessed");
-        }
-        else if (watchEvent->mask & IN_CLOSE_WRITE) {
-            strcat(notificationMessage, "written and closed");
-        }
-        else if (watchEvent->mask & IN_MODIFY) {
+        } else if (watchEvent->mask & IN_MODIFY) {
             strcat(notificationMessage, "modified");
-        }
-        else if (watchEvent->mask & IN_MOVE_SELF) {
-            strcat(notificationMessage, "moved");
-        }
-        else {
-            strcat(notificationMessage, "unknown event"); // handle other events
+        } else {
+            strcat(notificationMessage, "unknown event");
         }
 
-        // compare time to make events to be handled by 1sec
-        if (difftime(currentTime, lastEventTime) >= 1) {  // handle event at least 1sec gap
-            lastEventTime = currentTime;
-
-            // write event log
-            log_event(notificationMessage);
-
-            // print to console
-            printf("%s\n", notificationMessage);
-        }
+        log_event(notificationMessage);
     }
 }
 
@@ -177,7 +151,7 @@ void* inotify_thread(void* arg) {
             break;
         }
 
-        for (char* buffPointer = buffer; buffPointer < buffer + readLength; ) {
+        for (char* buffPointer = buffer; buffPointer < buffer + readLength;) {
             watchEvent = (const struct inotify_event*)buffPointer;
             process_event(watchEvent);
             buffPointer += sizeof(struct inotify_event) + watchEvent->len;
@@ -186,118 +160,102 @@ void* inotify_thread(void* arg) {
     return NULL;
 }
 
-void on_destroy(GtkWidget* widget, gpointer data) {
-    if (IeventQueue != -1) {
-        close(IeventQueue);
+void add_watch_recursive(const char* path) {
+    DIR* dir = opendir(path);
+    if (!dir) {
+        perror("Error opening directory");
+        return;
     }
-    gtk_main_quit();
+
+    struct dirent* entry;
+    while ((entry = readdir(dir)) != NULL) {
+        if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0) {
+            continue;
+        }
+
+        char subPath[512];
+        snprintf(subPath, sizeof(subPath), "%s/%s", path, entry->d_name);
+
+        struct stat pathStat;
+        if (stat(subPath, &pathStat) == 0 && S_ISDIR(pathStat.st_mode)) {
+            add_watch_recursive(subPath);
+        }
+    }
+
+    int wd = inotify_add_watch(IeventQueue, path, IN_CREATE | IN_DELETE | IN_MODIFY);
+    if (wd == -1) {
+        fprintf(stderr, "Error adding watch for %s\n", path);
+    } else {
+        printf("Watching: %s\n", path);
+    }
+
+    closedir(dir);
 }
 
-GtkWidget* create_window(const char* path) {
-    // create main window
+GtkWidget* create_window() {
     GtkWidget* window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
     gtk_window_set_title(GTK_WINDOW(window), "File Monitor");
     gtk_window_set_default_size(GTK_WINDOW(window), 800, 600);
 
-    // divide window
     GtkWidget* paned = gtk_paned_new(GTK_ORIENTATION_HORIZONTAL);
-
-    // create text space (scroll available) : left
     GtkWidget* scrolled_log = gtk_scrolled_window_new(NULL, NULL);
     GtkWidget* textView = gtk_text_view_new();
     gtk_text_view_set_editable(GTK_TEXT_VIEW(textView), FALSE);
     gtk_container_add(GTK_CONTAINER(scrolled_log), textView);
-
-    // get text buffer and show text at the top
     log_buffer = gtk_text_view_get_buffer(GTK_TEXT_VIEW(textView));
-    if(log_buffer) {
-	GtkTextIter start;
-	gtk_text_buffer_get_start_iter(log_buffer, &start);
-	gtk_text_buffer_insert(log_buffer, &start, "waiting for events...\n", -1);
-    }
 
-    // dir files : right
     GtkWidget* scrolled_files = gtk_scrolled_window_new(NULL, NULL);
     file_list = gtk_tree_view_new();
-
-    // set dir model
     GtkListStore* store = gtk_list_store_new(1, G_TYPE_STRING);
     gtk_tree_view_set_model(GTK_TREE_VIEW(file_list), GTK_TREE_MODEL(store));
-
-    // add colum (file name)
     GtkCellRenderer* renderer = gtk_cell_renderer_text_new();
     GtkTreeViewColumn* column = gtk_tree_view_column_new_with_attributes("Files", renderer, "text", 0, NULL);
     gtk_tree_view_append_column(GTK_TREE_VIEW(file_list), column);
     gtk_container_add(GTK_CONTAINER(scrolled_files), file_list);
 
-    // add to Paned
     gtk_paned_pack1(GTK_PANED(paned), scrolled_log, TRUE, FALSE);
     gtk_paned_pack2(GTK_PANED(paned), scrolled_files, TRUE, FALSE);
-
-    // add to window Paned
     gtk_container_add(GTK_CONTAINER(window), paned);
 
-    // initialize file list
-    update_file_list(path);
-
     return window;
-
 }
 
 int main(int argc, char** argv) {
-    char* basePath = NULL;
-
-    const uint32_t watchMask = IN_CREATE | IN_DELETE | IN_ACCESS | IN_CLOSE_WRITE | IN_MODIFY | IN_MOVE_SELF; // list of events
-
     if (argc < 2) {
-        fprintf(stderr, "USAGE: file_monitor PATH\n");
+        fprintf(stderr, "USAGE: file_monitor CONFIG_PATH\n");
         exit(EXT_ERR_TOO_FEW_ARGS);
     }
 
-    strncpy(monitored_path, argv[1], sizeof(monitored_path) - 1);
-    monitored_path[sizeof(monitored_path) - 1] = '\0';
+    read_config(argv[1]);
+    init_log_file(logFilePath);
 
-    // log file initialize and read config file
-    logFilePath = "file_monitor.log"; // set log file path (default)
-
-    rotate_log_file(logFilePath); // log file initialize
-
-    basePath = (char*)malloc(sizeof(char) * (strlen(argv[1]) + 1));
-    strcpy(basePath, argv[1]); // set directory path to watch
-
-    IeventQueue = inotify_init(); // inotify initialize
+    IeventQueue = inotify_init();
     if (IeventQueue == -1) {
         fprintf(stderr, "Error initializing inotify instance\n");
         exit(EXT_ERR_INIT_INOTIFY);
     }
 
-    IeventStatus = inotify_add_watch(IeventQueue, argv[1], watchMask); // add directory watch
-    if (IeventStatus == -1) {
-        fprintf(stderr, "Error adding file to watchlist\n");
-        exit(EXT_ERR_ADD_WATCH);
+    for (int i = 0; i < monitoredDirCount; ++i) {
+        add_watch_recursive(monitoredDirs[i]);
     }
 
-    // initialize gtk
     gtk_init(&argc, &argv);
 
-    GtkWidget* window = create_window(argv[1]);
-    g_signal_connect(window, "destroy", G_CALLBACK(on_destroy), NULL);
+    GtkWidget* window = create_window();
+    g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
 
-    // start inotify event handling thread
     pthread_t thread;
     if (pthread_create(&thread, NULL, inotify_thread, NULL) != 0) {
         perror("Error creating inotify thread");
         return EXIT_FAILURE;
     }
 
-    // gtk main loop
     gtk_widget_show_all(window);
     gtk_main();
 
-    // clean up
     pthread_cancel(thread);
     pthread_join(thread, NULL);
-    if(logFile) fclose(logFile);
 
+    if (logFile) fclose(logFile);
     return EXT_SUCCESS;
 }
--- file_monitor.c
+++ file_monitor.c
@@ -15,54 +15,32 @@
 #include <gtk/gtk.h>
 #include <glib.h>
 
-// define error code
 #define EXT_SUCCESS 0
 #define EXT_ERR_TOO_FEW_ARGS 1
 #define EXT_ERR_INIT_INOTIFY 2
 #define EXT_ERR_ADD_WATCH 3
-#define EXT_ERR_BASE_PATH_NULL 4
 #define EXT_ERR_READ_INOTIFY 5
-#define EXT_ERR_LIBNOTIFY 6
+#define EXT_ERR_CONFIG_FILE 6
 
-// global variables 
-int IeventStatus = -1; // inotify watch status
-int IeventQueue = -1; // inotify waiting queue
-char* ProgramTitle = "file_monitor"; // title of program
-FILE* logFile = NULL; // log file pointer
-char filteredExtension[64] = "";   // file extension to filter
-// int emailEnabled = 0; // email notificaton activate status
-// const char* emailRecipient = NULL; // email receiver
-const char* logFilePath = NULL; // log file path
-time_t lastEventTime = 0; // last event occur time
+int IeventStatus = -1;
+int IeventQueue = -1;
+char* ProgramTitle = "file_monitor";
+FILE* logFile = NULL;
+GtkTextBuffer *log_buffer = NULL;
+GtkWidget* file_list = NULL;
+char monitoredDirs[512][512];
+int monitoredDirCount = 0;
+char logFilePath[512] = "";
 
-GtkTextBuffer *log_buffer = NULL; // log text buffer
-GtkWidget* file_list = NULL; // directory to monitor
-char monitored_path[512];
-
-typedef struct {
-    int wd;                           // watch descriptor
-    char path[512];                   // directory path
-} WatchDescriptor;
-
-WatchDescriptor watchDescriptors[512];  // watch descriptor array
-int watchDescriptorCount = 0;           // number of watch descriptor assigned
-
-// recurrent function and log file size check
-void rotate_log_file(const char* logPath) {
-    if (logFile) {
-        fclose(logFile);
-    }
-
-    // open new log file
-    logFile = fopen(logPath, "a");
+void init_log_file(const char* path) {
+    logFile = fopen(path, "a");
     if (!logFile) {
         perror("Error opening log file");
         exit(EXIT_FAILURE);
     }
-    printf("Log file created/opened at: %s\n", logPath); 
+    printf("Log file initialized at: %s\n", path);
 }
 
-// callback func to use in g_idle_add() func
 gboolean update_log_buffer(gpointer data) {
     const char* eventMessage = (const char*)data;
 
@@ -74,59 +52,61 @@ gboolean update_log_buffer(gpointer data) {
     }
 
     g_free(data);
-
     return FALSE;
 }
 
-// event logging function
 void log_event(const char* eventMessage) {
     if (logFile) {
-         fprintf(logFile, "Event: %s\n", eventMessage); 
+        fprintf(logFile, "%s\n", eventMessage);
         fflush(logFile);
     }
 
-    // duplicate string and add to main thread
     g_idle_add(update_log_buffer, g_strdup(eventMessage));
 }
 
-// signal handle function: clean up after program ends
-void signal_handler(int signal) {
-    printf("Signal received: cleaning up\n");
+void read_config(const char* configPath) {
+    config_t cfg;
+    config_init(&cfg);
 
-    // remove inotify watch
-    if (inotify_rm_watch(IeventQueue, IeventStatus) == -1) {
-        fprintf(stderr, "Error removing from watch queue!\n");
-    }
-    close(IeventQueue); // ends inotify waiting queue
-
-    // close log file
-    if (logFile) {
-        fclose(logFile);
+    if (!config_read_file(&cfg, configPath)) {
+        fprintf(stderr, "Error reading config file %s: %s\n", configPath, config_error_text(&cfg));
+        config_destroy(&cfg);
+        exit(EXT_ERR_CONFIG_FILE);
     }
 
-    exit(EXIT_SUCCESS);
+    const char* logPath = NULL;
+    if (config_lookup_string(&cfg, "log_file", &logPath)) {
+        strncpy(logFilePath, logPath, sizeof(logFilePath));
+    } else {
+        fprintf(stderr, "Missing 'log_file' in config file\n");
+        config_destroy(&cfg);
+        exit(EXT_ERR_CONFIG_FILE);
+    }
+
+    config_setting_t* directories = config_lookup(&cfg, "monitor_directories");
+    if (directories && config_setting_is_array(directories)) {
+        monitoredDirCount = config_setting_length(directories);
+        for (int i = 0; i < monitoredDirCount && i < 512; ++i) {
+            const char* dir = config_setting_get_string_elem(directories, i);
+            strncpy(monitoredDirs[i], dir, sizeof(monitoredDirs[i]));
+        }
+    } else {
+        fprintf(stderr, "Missing or invalid 'monitor_directories' in config file\n");
+        config_destroy(&cfg);
+        exit(EXT_ERR_CONFIG_FILE);
+    }
+
+    config_destroy(&cfg);
 }
 
 void update_file_list(const char* path) {
-    if (!file_list) {
-        fprintf(stderr, "Error: file_list is not initialized\n");
-        return;
-    }
-
-    GtkTreeModel* model = GTK_LIST_STORE(gtk_tree_view_get_model(GTK_TREE_VIEW(file_list)));
-    if (GTK_IS_LIST_STORE(model)) {
-        fprintf(stderr, "Error: file_list does not have a valid GtkListStore\n");
-        return;
-    }
-
-    GtkListStore* store = GTK_LIST_STORE(model);
+    GtkListStore* store = GTK_LIST_STORE(gtk_tree_view_get_model(GTK_TREE_VIEW(file_list)));
     gtk_list_store_clear(store);
 
     DIR* dir = opendir(path);
     if (dir) {
         struct dirent* entry;
         while ((entry = readdir(dir)) != NULL) {
-            // exclude '.' & '..'
             if (strcmp(entry->d_name, ".") != 0 && strcmp(entry->d_name, "..") != 0) {
                 GtkTreeIter iter;
                 gtk_list_store_append(store, &iter);
@@ -134,57 +114,29 @@ void update_file_list(const char* path) {
             }
         }
         closedir(dir);
-    } else {
-        fprintf(stderr, "Error: Could not open directory %s\n", path);
     }
 }
 
-// event handle fucntion
 void process_event(const struct inotify_event* watchEvent) {
-    if (watchEvent->len > 0) { // handle if file has a name
+    if (watchEvent->len > 0) {
         char notificationMessage[512];
         char eventTime[64];
         time_t currentTime = time(NULL);
 
-        // format occur time
         strftime(eventTime, sizeof(eventTime), "%Y-%m-%d %H:%M:%S", localtime(&currentTime));
-        snprintf(notificationMessage, sizeof(notificationMessage), "[%s] File %s: ", eventTime, watchEvent->name); // create message include time
+        snprintf(notificationMessage, sizeof(notificationMessage), "[%s] File %s: ", eventTime, watchEvent->name);
 
-        // add message by event type
         if (watchEvent->mask & IN_CREATE) {
             strcat(notificationMessage, "created");
-            g_idle_add((GSourceFunc)update_file_list, g_strdup(monitored_path));
-        }
-        else if (watchEvent->mask & IN_DELETE) {
+        } else if (watchEvent->mask & IN_DELETE) {
             strcat(notificationMessage, "deleted");
-            g_idle_add((GSourceFunc)update_file_list, g_strdup(monitored_path));
-        }
-        else if (watchEvent->mask & IN_ACCESS) {
-            strcat(notificationMessage, "accessed");
-        }
-        else if (watchEvent->mask & IN_CLOSE_WRITE) {
-            strcat(notificationMessage, "written and closed");
-        }
-        else if (watchEvent->mask & IN_MODIFY) {
+        } else if (watchEvent->mask & IN_MODIFY) {
             strcat(notificationMessage, "modified");
-        }
-        else if (watchEvent->mask & IN_MOVE_SELF) {
-            strcat(notificationMessage, "moved");
-        }
-        else {
-            strcat(notificationMessage, "unknown event"); // handle other events
+        } else {
+            strcat(notificationMessage, "unknown event");
         }
 
-        // compare time to make events to be handled by 1sec
-        if (difftime(currentTime, lastEventTime) >= 1) {  // handle event at least 1sec gap
-            lastEventTime = currentTime;
-
-            // write event log
-            log_event(notificationMessage);
-
-            // print to console
-            printf("%s\n", notificationMessage);
-        }
+        log_event(notificationMessage);
     }
 }
 
@@ -199,7 +151,7 @@ void* inotify_thread(void* arg) {
             break;
         }
 
-        for (char* buffPointer = buffer; buffPointer < buffer + readLength; ) {
+        for (char* buffPointer = buffer; buffPointer < buffer + readLength;) {
             watchEvent = (const struct inotify_event*)buffPointer;
             process_event(watchEvent);
             buffPointer += sizeof(struct inotify_event) + watchEvent->len;
@@ -208,131 +160,102 @@ void* inotify_thread(void* arg) {
     return NULL;
 }
 
-void on_destroy(GtkWidget* widget, gpointer data) {
-    if (IeventQueue != -1) {
-        close(IeventQueue);
-    }
-    gtk_main_quit();
-}
-
-void on_open_directory_clicked(GtkWidget *button, gpointer user_data) {
-    GtkWidget *dialog;
-    GtkWindow *parent = GTK_WINDOW(user_data);
-
-    // Create a FileChooserDialog
-    dialog = gtk_file_chooser_dialog_new("Open Directory",
-                                         parent,
-                                         GTK_FILE_CHOOSER_ACTION_SELECT_FOLDER,
-                                         "_Cancel", GTK_RESPONSE_CANCEL,
-                                         "_Open", GTK_RESPONSE_ACCEPT,
-                                         NULL);
-
-    if (gtk_dialog_run(GTK_DIALOG(dialog)) == GTK_RESPONSE_ACCEPT) {
-        char *folder;
-        folder = gtk_file_chooser_get_filename(GTK_FILE_CHOOSER(dialog));
-        strncpy(monitored_path, folder, sizeof(monitored_path) - 1);
-        monitored_path[sizeof(monitored_path) - 1] = '\0';
-
-        printf("Selected directory: %s\n", monitored_path);
-        g_free(folder);
+void add_watch_recursive(const char* path) {
+    DIR* dir = opendir(path);
+    if (!dir) {
+        perror("Error opening directory");
+        return;
     }
 
-    gtk_widget_destroy(dialog);
+    struct dirent* entry;
+    while ((entry = readdir(dir)) != NULL) {
+        if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0) {
+            continue;
+        }
+
+        char subPath[512];
+        snprintf(subPath, sizeof(subPath), "%s/%s", path, entry->d_name);
+
+        struct stat pathStat;
+        if (stat(subPath, &pathStat) == 0 && S_ISDIR(pathStat.st_mode)) {
+            add_watch_recursive(subPath);
+        }
+    }
+
+    int wd = inotify_add_watch(IeventQueue, path, IN_CREATE | IN_DELETE | IN_MODIFY);
+    if (wd == -1) {
+        fprintf(stderr, "Error adding watch for %s\n", path);
+    } else {
+        printf("Watching: %s\n", path);
+    }
+
+    closedir(dir);
 }
 
-GtkWidget* create_window(const char* path) {
-    GtkWidget *window, *vbox, *button, *scrolled_log, *text_view;
-
-    // create main window
-    window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+GtkWidget* create_window() {
+    GtkWidget* window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
     gtk_window_set_title(GTK_WINDOW(window), "File Monitor");
     gtk_window_set_default_size(GTK_WINDOW(window), 800, 600);
 
-    // divide window
-    vbox = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 5);
+    GtkWidget* paned = gtk_paned_new(GTK_ORIENTATION_HORIZONTAL);
+    GtkWidget* scrolled_log = gtk_scrolled_window_new(NULL, NULL);
+    GtkWidget* textView = gtk_text_view_new();
+    gtk_text_view_set_editable(GTK_TEXT_VIEW(textView), FALSE);
+    gtk_container_add(GTK_CONTAINER(scrolled_log), textView);
+    log_buffer = gtk_text_view_get_buffer(GTK_TEXT_VIEW(textView));
 
-    // add a button to open the directory chooser dialog
-    button = gtk_button_new_with_label("Open Directory");
-    g_signal_connect(button, "clicked", G_CALLBACK(on_open_directory_clicked), window);
-    gtk_box_pack_start(GTK_BOX(vbox), button, FALSE, FALSE, 5);
+    GtkWidget* scrolled_files = gtk_scrolled_window_new(NULL, NULL);
+    file_list = gtk_tree_view_new();
+    GtkListStore* store = gtk_list_store_new(1, G_TYPE_STRING);
+    gtk_tree_view_set_model(GTK_TREE_VIEW(file_list), GTK_TREE_MODEL(store));
+    GtkCellRenderer* renderer = gtk_cell_renderer_text_new();
+    GtkTreeViewColumn* column = gtk_tree_view_column_new_with_attributes("Files", renderer, "text", 0, NULL);
+    gtk_tree_view_append_column(GTK_TREE_VIEW(file_list), column);
+    gtk_container_add(GTK_CONTAINER(scrolled_files), file_list);
 
-    // create text space (scroll available) : left
-    scrolled_log = gtk_scrolled_window_new(NULL, NULL);
-    text_view = gtk_text_view_new();
-    gtk_text_view_set_editable(GTK_TEXT_VIEW(text_view), FALSE);
-    gtk_container_add(GTK_CONTAINER(scrolled_log), text_view);
-
-    // get text buffer and show text at the top
-    log_buffer = gtk_text_view_get_buffer(GTK_TEXT_VIEW(text_view));
-    if(log_buffer) {
-	GtkTextIter start;
-	gtk_text_buffer_get_start_iter(log_buffer, &start);
-	gtk_text_buffer_insert(log_buffer, &start, "waiting for events...\n", -1);
-    }
-
-    // Add the vbox to the window
-    gtk_container_add(GTK_CONTAINER(window), vbox);
-
-    update_file_list(path);
+    gtk_paned_pack1(GTK_PANED(paned), scrolled_log, TRUE, FALSE);
+    gtk_paned_pack2(GTK_PANED(paned), scrolled_files, TRUE, FALSE);
+    gtk_container_add(GTK_CONTAINER(window), paned);
 
     return window;
-
 }
 
 int main(int argc, char** argv) {
-    char* basePath = NULL;
-
-    const uint32_t watchMask = IN_CREATE | IN_DELETE | IN_ACCESS | IN_CLOSE_WRITE | IN_MODIFY | IN_MOVE_SELF; // list of events
-
     if (argc < 2) {
-        fprintf(stderr, "USAGE: file_monitor PATH\n");
+        fprintf(stderr, "USAGE: file_monitor CONFIG_PATH\n");
         exit(EXT_ERR_TOO_FEW_ARGS);
     }
 
-    strncpy(monitored_path, argv[1], sizeof(monitored_path) - 1);
-    monitored_path[sizeof(monitored_path) - 1] = '\0';
+    read_config(argv[1]);
+    init_log_file(logFilePath);
 
-    // log file initialize and read config file
-    logFilePath = "file_monitor.log"; // set log file path (default)
-
-    rotate_log_file(logFilePath); // log file initialize
-
-    basePath = (char*)malloc(sizeof(char) * (strlen(argv[1]) + 1));
-    strcpy(basePath, argv[1]); // set directory path to watch
-
-    IeventQueue = inotify_init(); // inotify initialize
+    IeventQueue = inotify_init();
     if (IeventQueue == -1) {
         fprintf(stderr, "Error initializing inotify instance\n");
         exit(EXT_ERR_INIT_INOTIFY);
     }
 
-    IeventStatus = inotify_add_watch(IeventQueue, argv[1], watchMask); // add directory watch
-    if (IeventStatus == -1) {
-        fprintf(stderr, "Error adding file to watchlist\n");
-        exit(EXT_ERR_ADD_WATCH);
+    for (int i = 0; i < monitoredDirCount; ++i) {
+        add_watch_recursive(monitoredDirs[i]);
     }
 
-    // initialize gtk
     gtk_init(&argc, &argv);
 
-    GtkWidget* window = create_window(argv[1]);
-    g_signal_connect(window, "destroy", G_CALLBACK(on_destroy), NULL);
+    GtkWidget* window = create_window();
+    g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
 
-    // start inotify event handling thread
     pthread_t thread;
     if (pthread_create(&thread, NULL, inotify_thread, NULL) != 0) {
         perror("Error creating inotify thread");
         return EXIT_FAILURE;
     }
 
-    // gtk main loop
     gtk_widget_show_all(window);
     gtk_main();
 
-    // clean up
     pthread_cancel(thread);
     pthread_join(thread, NULL);
-    if(logFile) fclose(logFile);
 
+    if (logFile) fclose(logFile);
     return EXT_SUCCESS;
 }
